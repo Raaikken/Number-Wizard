004340786325f3969ee795479d57969c3894f9fa
Riku Kurjenrauma <riku.kurjenrauma@gmail.com>
2020-03-30 10:17:13 +0300

    Added necessary scenes

---
+++ Assets/SceneID.cs
@@ -0,0 +1,5 @@
+﻿public enum SceneID {
+	MainMenu,
+	GameScene,
+	EndScene
+}
---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Get current scene and load the next one
+	// Or main menu if at the end scene
+	public void LoadNextScene() {
+		if ((SceneManager.GetActiveScene().buildIndex + 1) <= 2) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+		else {
+			SceneManager.LoadScene((int)SceneID.MainMenu);
+		}
+	}
+}
